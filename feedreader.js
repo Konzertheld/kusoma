a0790ee825a1c3c77b9366c5f2838d2f2fef1a6f
Konzertheld <christian.gredig@gmail.com>
2013-04-14 02:02:33 +0200

    Have fun if you come across this in a Blame. Javascript reworks, basically fixing bugs with scrolling when navigating with keys. We now do that with an animation so we can have a callback that works around the fact that JS handles events that are triggered during code execution AFTER the code was executed to the end.

--- feedreader.js
+++ feedreader.js
@@ -1,6 +1,7 @@
 var FeedReader = {
 	request: null,
 	url: '',
+	nextFocus: undefined,
 
 	toggle: function(articleid)
 	{
@@ -44,64 +45,79 @@ var FeedReader = {
 	{
 		var prev = $("#post-" + articleid).prevAll(".unread");
 		if(prev.length > 0) { window.location.hash = "#" + prev.first().attr("id");	}
-	}
+	},
+	
+	unfocusAll: function() { $(".post.active").removeClass("active"); },
+	focus: function(post) { FeedReader.unfocusAll(); post.addClass("active"); }
 };
 
 // Scroll logic for marking posts as active
 $(window).scroll(function() {
-	var activepost = $(".post.active").first();
+	if($(window).scrollTop() == 0 || $(window).scrollTop() + $(window).height() == $(document).height()) {
+		// Work around the workaround. If we jump to the top/end with HOME/END after we used key navigation at the top/end so this function was not triggered, the post to force-focus is not used and has to be removed. Otherwise it would ruin the top/end jump.
+		FeedReader.nextFocus = undefined;
+	}
 	if($(window).scrollTop() == 0 && ($(".post").first().offset().top + $(".post").first().height()) > $(window).height()) {
 		// Make sure the focus is always on the top post if the window is scrolled to the very top AND the top post's bottom is not visible.
 		// Fixes probems with POS1/HOME key for large posts and avoids focus jumping for short posts
-		$(".post.active").each(function() { $(this).removeClass("active"); });
+		FeedReader.unfocusAll();
 		$(".post").first().addClass("active");
 		return;
 	}
-	if(($(window).scrollTop() + $(window).height() == $(document).height()) && ($(".post").last().offset().top < $(window).scrollTop())) {
+	if($(window).scrollTop() + $(window).height() == $(document).height() && ($(".post").last().offset().top < $(window).scrollTop())) {
 		// Make sure the focus is on the last post if the window is scrolled to the very end AND the last post's top is not visible.
 		// Fixes problems with END key for large posts and avoids focus jumping for short posts
-		$(".post.active").each(function() { $(this).removeClass("active"); });
+		FeedReader.unfocusAll();
 		$(".post").last().addClass("active");
 		return;
 	}
-	if(($(window).scrollTop() + $(window).height()) - (activepost.offset().top + activepost.height()) > ($(window).height() / 2)) {
-		// The active post's bottom is visible and has passed more than half of the window's height, move focus
-		if(activepost.next(".post").length > 0) {
-			activepost.removeClass("active");
-			activepost.next(".post").addClass("active");
-		}
-	}
-	if(($(window).scrollTop() + $(window).height() - activepost.offset().top) < $(window).height() / 2) {
-		// The active post has left the window to the bottom, move focus
-		if(activepost.prev(".post").length > 0) {
-			activepost.removeClass("active");
-			activepost.prev(".post").addClass("active");
-		}
-	}
-	activepost = $(".post.active").first();
-	if(activepost.offset().top + activepost.height() < $(window).scrollTop() || activepost.offset().top > $(window).scrollTop() + $(window).height())  {
-		// The active post has left the visible area completely without triggering one of the other conditions
-		// That happens if you use the HOME or END key and your posts are small. In that case we just figure out a new active post
-		$(".post.active").each(function() { $(this).removeClass("active"); });
-		$(".post").each(function() {
-			if($(this).offset().top > $(window).scrollTop() && $(this).offset().top < $(window).scrollTop() + $(window).height()) {
-				// We found a post that has the top edge in the visible area
-				$(this).addClass("active");
-				return false;
+	if($(".post.active").length > 0) {
+		var activepost = $(".post.active").first();
+		if(($(window).scrollTop() + $(window).height()) - (activepost.offset().top + activepost.height()) > ($(window).height() / 2)) {
+			// The active post's bottom is visible and has passed more than half of the window's height, move focus
+			if(activepost.next(".post").length > 0) {
+				activepost.removeClass("active");
+				activepost.next(".post").addClass("active");
 			}
-		});
-		if($(".post.active").length == 0) {
-			// We did not find a post with the top edge in the visible area, let's look for one with the bottom edge visible
-			// If we did that at the same time the results would be very odd
+		}
+		if(($(window).scrollTop() + $(window).height() - activepost.offset().top) < $(window).height() / 2) {
+			// The active post has left the window to the bottom, move focus
+			if(activepost.prev(".post").length > 0) {
+				activepost.removeClass("active");
+				activepost.prev(".post").addClass("active");
+			}
+		}
+		activepost = $(".post.active").first();
+		if(activepost.offset().top + activepost.height() < $(window).scrollTop() || activepost.offset().top > $(window).scrollTop() + $(window).height())  {
+			// The active post has left the visible area completely without triggering one of the other conditions
+			// That happens if you use the HOME or END key and your posts are small. In that case we just figure out a new active post
+			FeedReader.unfocusAll();
 			$(".post").each(function() {
-				if($(this).offset().top + $(this).height() < $(window).scrollTop() + $(window).height() && $(this).offset().top + $(this).height() > $(window).scrollTop()) {
+				if($(this).offset().top > $(window).scrollTop() && $(this).offset().top < $(window).scrollTop() + $(window).height()) {
+					// We found a post that has the top edge in the visible area
 					$(this).addClass("active");
 					return false;
 				}
 			});
+			if($(".post.active").length == 0) {
+				// We did not find a post with the top edge in the visible area, let's look for one with the bottom edge visible
+				// If we did that at the same time the results would be very odd
+				$(".post").each(function() {
+					if($(this).offset().top + $(this).height() < $(window).scrollTop() + $(window).height() && $(this).offset().top + $(this).height() > $(window).scrollTop()) {
+						$(this).addClass("active");
+						return false;
+					}
+				});
+			}
 		}
 	}
+	// Workaround for programmatically triggered scrolling, which will always trigger this event, even after the async JQuery animation
+	if(typeof FeedReader.nextFocus != 'undefined') {
+		FeedReader.focus(FeedReader.nextFocus);
+		FeedReader.nextFocus = undefined;
+	}
 	// This is the logic's end. If you manage to get your active post out of sight so that a long post fills the entire view having neither top nor bottom in the view... well, then you're screwed.
+	// If there is no active post in the second part, that's propably because some other logic is working. Don't care then.
 });
 
 document.onkeypress = function(event) {
@@ -116,12 +132,12 @@ document.onkeypress = function(event) {
 		case 106:
 		case 74:
 			// j = next
-			var active = $(".active").first();
-			FeedReader.jumpUnread(pid(active));
-			// Make sure the focus is moved even if the window is not scrolled
-			if(active.nextAll(".unread").length > 0) {
-				active.removeClass("active");
-				active.nextAll(".unread").first().addClass("active");
+			var next = $(".post.active").next(".unread");
+			if(next.length > 0) {
+				$("body,html").animate({scrollTop: next.offset().top - 20}, 100, 'linear', function() {
+					FeedReader.focus(next);
+					FeedReader.nextFocus = next;
+				});
 			}
 			break;
 		case 105:
@@ -133,18 +149,18 @@ document.onkeypress = function(event) {
 		case 107:
 		case 75:
 			// k = previous
-			var active = $(".active").first();
-			FeedReader.jumpUnreadBack(pid(active));
-			// Make sure the focus is moved even if the window is not scrolled
-			if(active.prevAll(".unread").length > 0) {
-				active.removeClass("active");
-				active.prevAll(".unread").first().addClass("active");
+			var prev = $(".post.active").prev(".unread");
+			if(prev.length > 0) {
+				$("body,html").animate({scrollTop: prev.offset().top - 20}, 100, 'linear', function() {
+					FeedReader.focus(prev);
+					FeedReader.nextFocus = prev;
+				});
 			}
 			break;
 		case 109:
 		case 77:
 			// m = toggle read
-			FeedReader.toggle(pid($(".active").first()));
+			FeedReader.toggle(pid($(".post.active")));
 			break;
 	}
 };
