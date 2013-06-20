8c10885e1c3d050b783bee82daf4996cdca2d6da
Konzertheld <christian.gredig@gmail.com>
2013-06-20 06:21:51 +0200

    Link the title to the source

--- post.php
+++ post.php
@@ -1,7 +1,7 @@
 <article id="post-<?php echo $content->id; ?>" class="post <?php echo Post::status_name($content->status); ?>" itemscope itemtype="http://schema.org/BlogPosting">
 
 	<header class="metadata">
-		<h1 itemprop="name"><a href="<?php echo $content->permalink; ?>" itemprop="url"><?php echo $content->title_out; ?></a></h1>
+		<h1 itemprop="name"><a href="<?php echo $content->info->link; ?>" itemprop="url"><?php echo $content->title_out; ?></a></h1>
 		<div class="pubdata">
 			<?php echo $content->toggle_status_link; ?><span itemprop="author" itemscope itemtype="http://schema.org/Person"><span itemprop="name"><?php echo $content->info->author; ?></span></span>
 			<time datetime="<?php echo $content->pubdate->format('Y-m-d\TH:i:s\Z'); ?>" itemprop="datePublished"><?php echo $content->pubdate->format(Options::get('dateformat') . ' ' . Options::get('timeformat')); ?></time>
